091da7a30811386155b23c825fc90d9afaecdc8a
Li Yaguang <yli2935@uwo.ca>
2024-12-02 12:49:59 -0400

    init

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,7 @@
+import { Tabs,Stack } from 'expo-router';
+export default function TabsLayout() {
+    return (
+        <Tabs>
+        </Tabs>
+    );
+}
---
+++ src/app/_layout.jsx
@@ -0,0 +1,7 @@
+import { Tabs,Stack } from 'expo-router';
+export default function RootLayout() {
+    return (
+        <Stack/>
+        
+    );
+}
